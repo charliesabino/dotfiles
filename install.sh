19c4b5cc7f1f010244ba7ac196f447091e84f12f
Charlie Sabino <94320712+charliesabino@users.noreply.github.com>
2024-12-22 19:06:23 -0500

    updates

---
+++ install.sh
@@ -0,0 +1,25 @@
+echo "Installing commandline tools..."
+xcode-select --install
+
+
+echo "Installing Brew..."
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew analytics off
+
+
+defaults write com.apple.dock static-only -bool true; killall Dock
+defaults write -g InitialKeyRepeat -int 10
+defaults write -g KeyRepeat -int 1
+
+brew install starship
+brew install ripgrep
+brew install lazygit
+brew install lsd
+brew install zoxide
+brew install neovim
+brew install --cask nikitabobko/tap/aerospace
+brew install --cask raycast
+brew install --cask alacritty
+brew install --cask zoom
+
+
