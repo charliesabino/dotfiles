27fdd2af8940410bba307a2c6ece83f9ca0c8cc9
Charlie Sabino <94320712+charliesabino@users.noreply.github.com>
2024-12-23 14:35:27 -0500

    Update install

--- install.sh
+++ install.sh
@@ -12,6 +12,8 @@ defaults write -g InitialKeyRepeat -int 10
 defaults write -g KeyRepeat -int 1
 defaults write -g ApplePressAndHoldEnabled -bool false
 
+brew install tmux
+brew install stow
 brew install starship
 brew install ripgrep
 brew install lazygit
@@ -20,7 +22,16 @@ brew install zoxide
 brew install neovim
 brew install fzf
 brew install bat
+brew install bitwarden-cli
 brew install --cask nikitabobko/tap/aerospace
 brew install --cask raycast
 brew install --cask alacritty
 brew install --cask zoom
+brew install --cask anki
+brew install --cask bartender
+brew tap shaunsingh/SFMono-Nerd-Font-Ligaturized
+brew install --cask font-sf-mono-nerd-font-ligaturized
+brew install --cask karabiner-elements
+brew install --cask bitwarden
+
+stow .
