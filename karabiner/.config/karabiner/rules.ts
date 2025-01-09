6783997cb7e91376ee41f1dbf517b6d271887faa
Charlie Sabino <94320712+charliesabino@users.noreply.github.com>
2025-01-09 17:26:22 -0600

    Add karabiner config

---
+++ karabiner/.config/karabiner/rules.ts
@@ -0,0 +1,239 @@
+import fs from "fs";
+import { KarabinerRules } from "./types";
+import { createHyperSubLayers, app, open, rectangle, shell } from "./utils";
+
+const rules: KarabinerRules[] = [
+  // Define the Hyper key itself
+  {
+    description: "Hyper Key (⌃⌥⇧⌘)",
+    manipulators: [
+      {
+        description: "Caps Lock -> Hyper Key",
+        from: {
+          key_code: "caps_lock",
+          modifiers: {
+            optional: ["any"],
+          },
+        },
+        to: [
+          {
+            set_variable: {
+              name: "hyper",
+              value: 1,
+            },
+          },
+        ],
+        to_after_key_up: [
+          {
+            set_variable: {
+              name: "hyper",
+              value: 0,
+            },
+          },
+        ],
+        to_if_alone: [
+          {
+            key_code: "escape",
+          },
+        ],
+        type: "basic",
+      },
+      //      {
+      //        type: "basic",
+      //        description: "Disable CMD + Tab to force Hyper Key usage",
+      //        from: {
+      //          key_code: "tab",
+      //          modifiers: {
+      //            mandatory: ["left_command"],
+      //          },
+      //        },
+      //        to: [
+      //          {
+      //            key_code: "tab",
+      //          },
+      //        ],
+      //      },
+    ],
+  },
+  ...createHyperSubLayers({
+    // b = "B"rowse
+    b: {
+      l: open("https://leetcode.com/problemset/"),
+    },
+    // o = "Open" applications
+    o: {
+      s: app("Superhuman"),
+      t: app("Alacritty"),
+      c: app("Notion Calendar"),
+      b: app("Arc"),
+      f: app("Finder"),
+      n: app("Notes"),
+      i: app("Messages"),
+      m: open("applenotes://showNote?identifier=9382DE40-DC79-4F38-8601-CF72F8D68648"),
+      a: open("Anki"),
+    },
+
+
+    // s = "System"
+    s: {
+      u: {
+        to: [
+          {
+            key_code: "volume_increment",
+          },
+        ],
+      },
+      j: {
+        to: [
+          {
+            key_code: "volume_decrement",
+          },
+        ],
+      },
+      i: {
+        to: [
+          {
+            key_code: "display_brightness_increment",
+          },
+        ],
+      },
+      k: {
+        to: [
+          {
+            key_code: "display_brightness_decrement",
+          },
+        ],
+      },
+      l: {
+        to: [
+          {
+            key_code: "q",
+            modifiers: ["right_control", "right_command"],
+          },
+        ],
+      },
+      p: {
+        to: [
+          {
+            key_code: "play_or_pause",
+          },
+        ],
+      },
+      semicolon: {
+        to: [
+          {
+            key_code: "fastforward",
+          },
+        ],
+      },
+      e: open(
+        `raycast://extensions/thomas/elgato-key-light/toggle?launchType=background`
+      ),
+      // "D"o not disturb toggle
+      d: open(
+        `raycast://extensions/yakitrak/do-not-disturb/toggle?launchType=background`
+      ),
+      // "T"heme
+      t: open(`raycast://extensions/raycast/system/toggle-system-appearance`),
+      c: open("raycast://extensions/raycast/system/open-camera"),
+      // 'v'oice
+      v: {
+        to: [
+          {
+            key_code: "spacebar",
+            modifiers: ["left_option"],
+          },
+        ],
+      },
+    },
+
+    // v = "moVe" which isn't "m" because we want it to be on the left hand
+    // so that hjkl work like they do in vim
+    v: {
+      h: {
+        to: [{ key_code: "left_arrow" }],
+      },
+      j: {
+        to: [{ key_code: "down_arrow" }],
+      },
+      k: {
+        to: [{ key_code: "up_arrow" }],
+      },
+      l: {
+        to: [{ key_code: "right_arrow" }],
+      },
+      // Scroll mode via homerow.app
+      s: {
+        to: [{ key_code: "j", modifiers: ["right_control"] }],
+      },
+      u: {
+        to: [{ key_code: "page_down" }],
+      },
+      i: {
+        to: [{ key_code: "page_up" }],
+      },
+    },
+
+
+    // test
+    // r = "Raycast"
+    r: {
+      q: open("raycast://extensions/mackopes/quit-applications/index"),
+      e: open(
+        "raycast://extensions/raycast/emoji-symbols/search-emoji-symbols"
+      ),
+      p: open("raycast://extensions/raycast/raycast/confetti"),
+      a: open("raycast://extensions/raycast/raycast-ai/ai-chat"),
+      s: open("raycast://extensions/peduarte/silent-mention/index"),
+      h: open(
+        "raycast://extensions/raycast/clipboard-history/clipboard-history"
+      ),
+      n: open("raycast://extensions/raycast/apple-notes/index"),
+    },
+  }),
+  {
+    description: "Change Backspace to Spacebar when Minecraft is focused",
+    manipulators: [
+      {
+        type: "basic",
+        from: {
+          key_code: "delete_or_backspace",
+        },
+        to: [
+          {
+            key_code: "spacebar",
+          },
+        ],
+        conditions: [
+          {
+            type: "frontmost_application_if",
+            file_paths: [
+              "^/Users/mxstbr/Library/Application Support/minecraft/runtime/java-runtime-gamma/mac-os-arm64/java-runtime-gamma/jre.bundle/Contents/Home/bin/java$",
+            ],
+          },
+        ],
+      },
+    ],
+  },
+];
+
+fs.writeFileSync(
+  "karabiner.json",
+  JSON.stringify(
+    {
+      global: {
+        show_in_menu_bar: false,
+      },
+      profiles: [
+        {
+          name: "Default",
+          complex_modifications: {
+            rules,
+          },
+        },
+      ],
+    },
+    null,
+    2
+  )
+);
