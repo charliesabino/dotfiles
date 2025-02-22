7a85e0767550cc34780b550e1806ec9e6aacb974
Charlie Sabino <94320712+charliesabino@users.noreply.github.com>
2025-02-22 10:00:34 -0600

    Small updates

--- karabiner/.config/karabiner/rules.ts
+++ karabiner/.config/karabiner/rules.ts
@@ -164,7 +164,7 @@ const rules: KarabinerRules[] = [
     v: {
       // Scroll mode via homerow.app
       s: {
-        to: [{ key_code: "f", modifiers: ["right_control"] }],
+        to: [{ key_code: "f", modifiers: ["shift", "right_control"] }],
       },
       u: {
         to: [{ key_code: "page_down" }],
--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,3 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- prevent esc + j/k from moving line
+-- https://www.reddit.com/r/lunarvim/comments/1334htt/lunarvim_in_tmux_moving_line_up_or_down_when/
+vim.opt.timeoutlen = 1000
+vim.opt.timeoutlen = 0
