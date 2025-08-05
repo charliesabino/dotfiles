8193a0111a3a2d9100b57d880e4da8690fa7fe84
Charlie Sabino <94320712+charliesabino@users.noreply.github.com>
2025-08-05 10:21:30 -0400

    Source env file

--- fish/config.fish
+++ fish/config.fish
@@ -37,8 +37,6 @@ atuin init fish --disable-up-arrow | source
 set -U fish_key_bindings fish_vi_key_bindings
 set -Ux EDITOR nvim
 
-# Added by OrbStack: command-line tools and integration
-# This won't be added again if you remove it.
-source ~/.orbstack/shell/init2.fish 2>/dev/null || :
+source ~/.env
 
 alias claude="/Users/charliesabino/.claude/local/claude"
