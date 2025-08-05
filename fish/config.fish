3bf56d650053dbcdb9b78689e1c553d5418ca457
Charlie Sabino <94320712+charliesabino@users.noreply.github.com>
2025-08-05 10:07:26 -0400

    Add claude

--- fish/config.fish
+++ fish/config.fish
@@ -40,3 +40,5 @@ set -Ux EDITOR nvim
 # Added by OrbStack: command-line tools and integration
 # This won't be added again if you remove it.
 source ~/.orbstack/shell/init2.fish 2>/dev/null || :
+
+alias claude="/Users/charliesabino/.claude/local/claude"
