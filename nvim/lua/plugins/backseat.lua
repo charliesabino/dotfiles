5abe92923152304c6427167fe071dcbeb6cff997
Charlie Sabino <94320712+charliesabino@users.noreply.github.com>
2025-09-01 19:29:39 +1000

    Comment out backseat

--- nvim/lua/plugins/backseat.lua
+++ nvim/lua/plugins/backseat.lua
@@ -1,11 +1,12 @@
-return {
-  dir = "/Users/charliesabino/Dev/backseat.nvim",
-  config = function()
-    require("backseat").setup({
-      anthropic_api_key = vim.env.ANTHROPIC_API_KEY,
-      gemini_api_key = vim.env.GEMINI_API_KEY,
-      analysis_interval = 15,
-      model = "gemini-2.5-flash-lite",
-    })
-  end,
-}
+-- return {
+--   dir = "/Users/charliesabino/Dev/backseat.nvim",
+--   config = function()
+--     require("backseat").setup({
+--       anthropic_api_key = vim.env.ANTHROPIC_API_KEY,
+--       gemini_api_key = vim.env.GEMINI_API_KEY,
+--       analysis_interval = 15,
+--       model = "gemini-2.5-flash-lite",
+--       enable_monitoring = false,
+--     })
+--   end,
+-- }
